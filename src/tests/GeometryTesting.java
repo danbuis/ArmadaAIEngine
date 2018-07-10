498d1819f48580e573e94368abe904332c9961ca
danbuis <dan.buis88@gmail.com>
2018-07-10 15:03:16 -0400

    Modified range to polygon test
    shifted 2nd ship vertically to ensure that the measurement isn't always
    point to point.  Since they are offset vertically AND horizontally, the
    closest measurement is point to edge, or even edge to edge, but
    definitely NOT point to point.

--- src/tests/GeometryTesting.java
+++ src/tests/GeometryTesting.java
@@ -10,11 +10,16 @@ import geometry.geometryHelper;
 public class GeometryTesting {
 
 	@Test
-	public void testHullZoneRangeMeasures() {
+	public void testRangeToPolygon() {
 		BasicShip ship1 = new BasicShip("Nebulon-B Support Frigate");
 		BasicShip ship2 = new BasicShip("Nebulon-B Support Frigate");
 		
+		//move one ship to the right
 		ship2.moveAndRotate(50, 0, 0);
+		assertEquals(9.0, geometryHelper.rangeToPolygon(ship1.getRight().getGeometry(), ship2.getLeft().getGeometry()), 0.001);
+		
+		//now move it up.  range should not change because the edges are parallel
+		ship2.moveAndRotate(0, 20, 0);
 		assertEquals(9.0, geometryHelper.rangeToPolygon(ship1.getRight().getGeometry(), ship2.getLeft().getGeometry()), 0.001);
 	}
 
