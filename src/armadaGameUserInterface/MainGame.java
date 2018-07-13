8198d1b30ea654af4cb8486bae0e1cba2ad2f50f
danbuis <dan.buis88@gmail.com>
2018-07-13 15:12:28 -0400

    Added Main Menu Screen
    No functionality yet

--- src/armadaGameUserInterface/MainGame.java
+++ src/armadaGameUserInterface/MainGame.java
@@ -5,17 +5,30 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class MainGame extends BasicGame
 {
+	public GameMenuState gameMenuState = GameMenuState.MAINMENU;
+	private Image demoGameButton;
+	private Image standardGameButton;
+	private Image fleetBuilderButton;
+	private Image mainMenuBG;
+	
 	public MainGame(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+		demoGameButton = new Image("Graphics/UI/DemoGameButton.png");
+		standardGameButton = new Image("Graphics/UI/StandardGameButton.png");
+		fleetBuilderButton = new Image("Graphics/UI/FleetBuilderButton.png");
+		mainMenuBG = new Image("Graphics/UI/MainScreenBG.png");
+		
+	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
@@ -23,7 +36,19 @@ public class MainGame extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Howdy!", 100, 100);
+		if(gameMenuState == GameMenuState.MAINMENU){
+			int width = mainMenuBG.getWidth();
+			int height = mainMenuBG.getHeight();
+			int buttonWidth = demoGameButton.getWidth();
+			int buttonHeight = demoGameButton.getHeight();
+			
+			
+			g.drawImage(mainMenuBG, 0, 0);
+			g.drawImage(demoGameButton, width/2-buttonWidth/2, height/2+buttonHeight);
+			g.drawImage(standardGameButton, width/2-buttonWidth/2, height/2-buttonHeight/2);
+			g.drawImage(fleetBuilderButton, width/2-buttonWidth/2, height/2-2*buttonHeight);
+						
+		}
 	}
 
 	public static void main(String[] args)
