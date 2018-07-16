1c17142f9cb5a0c647b1d783ec7172abd4eb7a18
danbuis <dan.buis88@gmail.com>
2018-07-16 14:45:46 -0400

    Functional Main Menu
    Renders game screen for both demo games and standard size games.

--- src/armadaGameUserInterface/MainGame.java
+++ src/armadaGameUserInterface/MainGame.java
@@ -1,19 +1,26 @@
 package armadaGameUserInterface;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class MainGame extends BasicGame
 {
 	public GameMenuState gameMenuState = GameMenuState.MAINMENU;
+	//UI components for main menu
 	private Image demoGameButton;
+	private Rectangle demoButtonRectangle;
 	private Image standardGameButton;
+	private Rectangle standardButtonRectangle;
 	private Image fleetBuilderButton;
+	private Rectangle fleetButtonRectangle;
 	private Image mainMenuBG;
 	
 	private int mainMenuButtonWidth;
@@ -21,6 +28,16 @@ public class MainGame extends BasicGame
 	private int totalWidth;
 	private int totalHeight;
 	
+	//UI components for game screen
+	private Image standardGameBorder;
+	private Image demoGameBorder;
+	private Image background;
+	private Image shipDetailWindow1;
+	private Image shipDetailWindow2;
+	private Image gameScreenBackground;
+	
+	private int shipDetailWindowWidth;
+	private int shipDetailWindowHeight;
 	
 	
 	public MainGame(String gamename)
@@ -32,30 +49,69 @@ public class MainGame extends BasicGame
 	public void init(GameContainer gc) throws SlickException {
 		demoGameButton = new Image("Graphics/UI/DemoGameButton.png");
 		standardGameButton = new Image("Graphics/UI/StandardGameButton.png");
-		fleetBuilderButton = new Image("Graphics/UI/FleetBuilderButton.png");
 		mainMenuBG = new Image("Graphics/UI/MainScreenBG.png");
 		
 		totalWidth = mainMenuBG.getWidth();
 		totalHeight = mainMenuBG.getHeight();
 		mainMenuButtonWidth = demoGameButton.getWidth();
 		mainMenuButtonHeight = demoGameButton.getHeight();
+		
+		demoButtonRectangle = new Rectangle(totalWidth/2-mainMenuButtonWidth/2, totalHeight/2+mainMenuButtonHeight, mainMenuButtonWidth, mainMenuButtonHeight);
+		standardButtonRectangle = new Rectangle(totalWidth/2-mainMenuButtonWidth/2, totalHeight/2-mainMenuButtonHeight/2, mainMenuButtonWidth, mainMenuButtonHeight);
+		fleetBuilderButton = new Image("Graphics/UI/FleetBuilderButton.png");
+		System.out.println("demo rectangle "+demoButtonRectangle.getCenterX()+","+demoButtonRectangle.getCenterY());
+
+		standardGameBorder = new Image("Graphics/UI/3x6border.png");
+		demoGameBorder = new Image("Graphics/UI/3x3border.png");
+		background = new Image("Graphics/UI/blackBackground.png");
+		shipDetailWindow1 = new Image("Graphics/UI/shipDetailWindow.png");
+		shipDetailWindow2 = new Image("Graphics/UI/shipDetailWindow.png");
+		gameScreenBackground = new Image("Graphics/UI/GameScreenBG.png");
+		
+		shipDetailWindowWidth = shipDetailWindow1.getWidth();
+		shipDetailWindowHeight = shipDetailWindow1.getHeight();
 	}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+		int mouseX = Mouse.getX();
+		int mouseY = Mouse.getY();
+		
+		if(gameMenuState == GameMenuState.MAINMENU){
+			if(demoButtonRectangle.contains(mouseX, mouseY)){
+				if(Mouse.isButtonDown(0)){
+					gameMenuState = GameMenuState.DEMOGAME;
+					System.out.println("demo button pressed");		
+				}
+			}else if(standardButtonRectangle.contains(mouseX, mouseY)){
+				if(Mouse.isButtonDown(0)){
+					gameMenuState = GameMenuState.REGULARGAME;
+					System.out.println("standard game button pressed");
+				}
+			}
+		}
+		
+	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
 		if(gameMenuState == GameMenuState.MAINMENU){
-			
-			
-			
+			g.drawString("Mouse loc "+Mouse.getX()+","+Mouse.getY(), 10, 10);
 			g.drawImage(mainMenuBG, 0, 0);
-			g.drawImage(demoGameButton, totalWidth/2-mainMenuButtonWidth/2, totalHeight/2+mainMenuButtonHeight);
+			g.drawImage(fleetBuilderButton, totalWidth/2-mainMenuButtonWidth/2, totalHeight/2+mainMenuButtonHeight);
 			g.drawImage(standardGameButton, totalWidth/2-mainMenuButtonWidth/2, totalHeight/2-mainMenuButtonHeight/2);
-			g.drawImage(fleetBuilderButton, totalWidth/2-mainMenuButtonWidth/2, totalHeight/2-2*mainMenuButtonHeight);
-						
+			g.drawImage(demoGameButton, totalWidth/2-mainMenuButtonWidth/2, totalHeight/2-2*mainMenuButtonHeight);			
+		}
+		
+		if(gameMenuState == GameMenuState.DEMOGAME || gameMenuState == GameMenuState.REGULARGAME){
+			g.drawImage(background, 10, totalHeight-background.getHeight()-200);
+			g.drawImage(gameScreenBackground, 0, 0);
+			g.drawImage(shipDetailWindow1, 270, totalHeight-shipDetailWindow1.getHeight()-20);
+			g.drawImage(shipDetailWindow2, 643, totalHeight-shipDetailWindow1.getHeight()-20);
+			
+			if(gameMenuState == GameMenuState.DEMOGAME) g.drawImage(demoGameBorder, 295, totalHeight-demoGameBorder.getHeight()-237);
+			else g.drawImage(standardGameBorder, 79, totalHeight-demoGameBorder.getHeight()-237);
 		}
 	}
 
